5263b3f24266b851f20a5e7b5d0426ba4e0b2a0d
Andres <andresmaradiaga707@gmail.com>
2024-09-16 10:16:12 -0600

    Actualizacion de vistas login y agregado de Vendor.rar

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -90,6 +90,13 @@
                             {{ __('Ver código QR') }}
                         </x-button>
                     </x-confirms-password>
+                    <form method="POST" action="{{ route('logout') }}" class="inline">
+                        @csrf
+            
+                        <button type="submit" class="ms-4 inline-flex items-center px-4 py-2 bg-white border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                            {{ __('Cancelar') }}
+                        </button>
+                    </form>
                 @else
                     @if ($showingRecoveryCodes)
                         <x-confirms-password wire:then="regenerateRecoveryCodes">
